1e305119c3b17d43ce7d839d71fbbc40bd7c7e45
Andres Montano <amontano@virginia.edu>
2010-03-05 17:11:42 +0600

    Fixed locations and names of libs

--- init.rb
+++ init.rb
@@ -1,7 +1,6 @@
 # Include hook code here
-require 'open-uri'
 require 'hpricot'
 require 'patches/traverse'
-require 'patches/active_resource_patch'
+require 'patches/active_resource'
 ActionView::Base.send :include, BasicEngineHelper
 ActionView::Base.send :include, TinyMceExtensionHelper
