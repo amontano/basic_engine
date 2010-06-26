c1c172cfe2e8b80e70b4a1ef0bfb796e452786a0
Andres Montano <amontano@virginia.edu>
2010-06-25 20:49:36 -0600

    moved common code to active resource extension

--- init.rb
+++ init.rb
@@ -1,6 +1,3 @@
 # Include hook code here
-require 'hpricot'
-require 'patches/traverse'
-require 'patches/active_resource'
 ActionView::Base.send :include, BasicEngineHelper
 ActionView::Base.send :include, TinyMceExtensionHelper
